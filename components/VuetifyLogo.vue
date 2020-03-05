777cbe8859380023b5a86139a13ea62d92832ee7
dev <alexmartos96@gmail.com>
2020-03-05 00:19:40 -0800

    images

--- components/VuetifyLogo.vue
+++ components/VuetifyLogo.vue
@@ -1,10 +1,10 @@
 <template>
-  <img class="VuetifyLogo" alt="Vuetify Logo" src="~/assets/penlogo.png" />
+  <img class="VuetifyLogo" alt="Vuetify Logo" src="~/assets/altitudelogo.png" />
 </template>
 
 <style>
 .VuetifyLogo {
-  width: 50px;
+  width: 100px;
   height: 50px;
   transform: rotateY(560deg);
   animation: turn 3.5s ease-out forwards 1s;
